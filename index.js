c547384f4c360190f00e9ec437906c1ea939c2c4
oscarmparras <o.m.parras@gmail.com>
2014-08-24 19:23:08 -0500

    Connecting to mongohq instance with local environment variables set using foreman. Pulling data and returning as json.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var express = require('express')
 var app = express();
 var cool = require('cool-ascii-faces');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk(process.env.MONGOHQ_URL);
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
+app.get('/helloworld', function(request, response) {
   response.json({ message: 'Hello World!', face: cool() });
+})
+
+app.get('/productcollection/all', function(request, response) {
+  var products = db.get('productcollection');
+  products.find({}, {}, function(error, docs){
+  	response.send(docs);
+  });
 })
 
 app.listen(app.get('port'), function() {
